2e5ee4db99b500997d9bae80bbd3d8c6b59b9578
Brandon <brandon@blackjoy.co>
2017-04-16 00:49:00 +0000

    more comments

--- testing.js
+++ testing.js
@@ -1,18 +1,18 @@
 //Defining the Angular Module
 var app1 = angular.module('app1', []);
 
-//Define the first controller
+//Define the first controller and the factory function
 app1.controller('math', function($scope) {
     $scope.first = 1;
     $scope.second = 1;
     $scope.third = 1;
     $scope.fourth = 1;
     
-    //
+    //Define the multiplyValue function that binds to a button
     $scope.multiplyValue = function () {
         $scope.product = $scope.first + ' * ' + $scope.second + " = " + (+$scope.first * +$scope.second);
     };
-    //
+     //Define the addValue function that binds to a button
      $scope.addValue = function () {
         $scope.sum = $scope.third + ' + ' + $scope.fourth + " = " + (+$scope.third + +$scope.fourth);
      };
