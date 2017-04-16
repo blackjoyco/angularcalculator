c760246743f8765fd54c736e81386a89bda3deba
Brandon <brandon@blackjoy.co>
2017-04-16 00:41:39 +0000

    add comments to app

--- testing.js
+++ testing.js
@@ -1,15 +1,18 @@
+//Defining the Angular Module
 var app1 = angular.module('app1', []);
 
-app1.controller('crtl1', function($scope) {
+//Define the first controller
+app1.controller('math', function($scope) {
     $scope.first = 1;
     $scope.second = 1;
     $scope.third = 1;
     $scope.fourth = 1;
-
+    
+    //
     $scope.multiplyValue = function () {
         $scope.product = $scope.first + ' * ' + $scope.second + " = " + (+$scope.first * +$scope.second);
     };
-    
+    //
      $scope.addValue = function () {
         $scope.sum = $scope.third + ' + ' + $scope.fourth + " = " + (+$scope.third + +$scope.fourth);
      };
